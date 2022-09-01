4eda8339656af3f1cce79e2c4045ada0eefbdb34
vidya <vidya@host.docker.internal>
2022-09-01 19:19:48 +0530

    I have added one more class...

---
+++ src/com/java/demoproject/Class2.java
@@ -0,0 +1,8 @@
+package com.java.demoproject;
+
+public class Class2 {
+	public void car() {
+		System.out.println("Message from Car....");
+	}
+
+}
--- src/com/java/demoproject/DemoClass.java
+++ src/com/java/demoproject/DemoClass.java
@@ -4,6 +4,8 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 	System.out.println("Hello from main class");
+	Class2 obj=new Class2();
+	obj.car();
 
 	}
 
