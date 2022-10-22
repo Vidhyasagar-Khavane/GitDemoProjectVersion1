f492acd71741ba4cd9f191370b568de5a18ae3b3
Vidhyasagar-Khavane <vidhyasagarkhavane@gmail.com>
2022-10-22 20:47:24 +0530

    class added by Y

---
+++ src/com/java/demoproject/ClassThree.java
@@ -0,0 +1,10 @@
+package com.java.demoproject;
+
+public class ClassThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test 3");
+	}
+
+}
