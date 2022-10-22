e3cfaaae13e0cfaf57eed7c7627fc6522ff34484
Vidhyasagar-Khavane <vidhyasagarkhavane@gmail.com>
2022-10-22 20:52:00 +0530

    added class four by X

---
+++ src/com/java/demoproject/ClassFour.java
@@ -0,0 +1,7 @@
+package com.java.demoproject;
+
+public class ClassFour {
+	public static void main(String[] args) {
+		System.out.println("Test 1");
+	}
+}
--- src/com/java/demoproject/ClassThree.java
+++ src/com/java/demoproject/ClassThree.java
@@ -5,6 +5,7 @@ public class ClassThree {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test 3");
+		System.out.println("Test 4");
 	}
 
 }
